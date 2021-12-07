7ce6ec6787010797e2cd790bf9fd9578c00a41d9
Abhishek Kashyap <abhik5567@gmail.com>
2021-12-07 23:33:26 +0530

    Changed game icon and title

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import pygame
+import pathlib
+
 pygame.init()
-screen = pygame.display.set_mode((800,600))
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load(r"C:\Users\user\Pictures\Games\2D\alien-ufo-pack\shipBlue_manned.png")
+pygame.display.set_icon(icon)
 run = True
+
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
