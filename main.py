5b67c7ba2c9a1a42cd69fc3da8f6815af660a61a
Abhishek Kashyap <abhik5567@gmail.com>
2021-12-08 22:41:23 +0530

    Added the laser shooting part

--- main.py
+++ main.py
@@ -20,31 +20,54 @@ def player(x, y):
 enemy_img = pygame.image.load("images/enemy.png")
 enemy_x = random.randint(0, 736)
 enemy_y = random.randint(50, 150)
-enemy_x_change = random.choice([0.3, -0.3])
+enemy_x_change = random.choice([3, -3])
 
 
 def enemy(x, y):
     screen.blit(enemy_img, (x, y))
 
 
-background_img = pygame.image.load("images/background.jpg")
+background_img = pygame.image.load("images/background.png")
+laser_img = pygame.image.load("images/laser.png")
+laser_x = 0
+laser_y = 500
+laser_y_change = 0
+laser_state = "static"
+
+
+def fire_laser(x, y):
+    global laser_state
+    laser_state = "dynamic"
+    
+    screen.blit(laser_img, (x, y))
+
+
 run = True
 
 player_x_change = 0
 while run:
+    screen.blit(background_img, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player_x_change = -0.5
+                player_x_change = -5
             elif event.key == pygame.K_RIGHT:
-                player_x_change = 0.5
+                player_x_change = 5
+            elif event.key == pygame.K_SPACE:
+                laser_y_change = -20
+                laser_y = 490
+                laser_x = player_x + 15
+                lasersound = pygame.mixer.Sound(r"C:\Users\user\Music\Digital Audio\laser1.ogg")
+                lasersound.play()
+                fire_laser(laser_x, laser_y)
         elif event.type == pygame.KEYUP:
             if event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
                 player_x_change = 0
     player_x += player_x_change
-    
+
     if player_x <= 0:
         player_x = 0
     elif player_x >= 736:
@@ -53,14 +76,21 @@ while run:
 
     if enemy_x <= 0:
         enemy_y += 64
-        enemy_x_change = 0.3
+        enemy_x_change = 3
     elif enemy_x >= 736:
         enemy_y += 64
-        enemy_x_change = -0.3
+        enemy_x_change = -3
     enemy_x += enemy_x_change
-    screen.fill((0, 0, 0))  # change bg color
+
+    if laser_y == 0:
+        pass
+    elif laser_y >=0:
+        laser_y += laser_y_change
+    else:  
+        laser_state = "static"
     
-    screen.blit(background_img, (0, 0))
     player(player_x, player_y)
     enemy(enemy_x, enemy_y)
+    if laser_state == "dynamic":
+        fire_laser(laser_x, laser_y)
     pygame.display.update()
